dd6f875b9dff6876321619a3723fc95fb5794958
locpd <32977243+phamduyloc@users.noreply.github.com>
2020-09-16 22:15:55 +0700

    Update Openperwebsite.py

--- Openperwebsite.py
+++ Openperwebsite.py
@@ -1,29 +1,70 @@
 import sys
-import requests
+import os
 import webbrowser
+import requests
+
+pathfile = sys.argv[1]
+numPageOpen = int(sys.argv[2])
+
+#Open new tab firefox voi url la url
+def openwebsite(url):
+	#webbrowser.get('firefox').open_new_tab(url)
+	webbrowser.get('google-chrome').open_new_tab(url)
 
 
-webbrowser.register('chrome', None,
-	webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
+if not os.path.isfile(pathfile):
+	print("File path {} does not exist. Exiting ...".format(pathfile))
+	sys.exit()
 
-pathFile=sys.argv[1]
-numPageOpen=sys.argv[2]
-numPageOpened=0
+try:
+	strArt = '''
+       	                                                                               $$$$$      
+    	                                                                               $:::$      
+	555555555555555555      000000000          000000000          000000000        $$$$$:::$$$$$$ 
+	5::::::::::::::::5    00:::::::::00      00:::::::::00      00:::::::::00    $$::::::::::::::$
+	5::::::::::::::::5  00:::::::::::::00  00:::::::::::::00  00:::::::::::::00 $:::::$$$$$$$::::$
+	5:::::555555555555 0:::::::000:::::::00:::::::000:::::::00:::::::000:::::::0$::::$       $$$$$
+	5:::::5            0::::::0   0::::::00::::::0   0::::::00::::::0   0::::::0$::::$            
+	5:::::5            0:::::0     0:::::00:::::0     0:::::00:::::0     0:::::0$::::$            
+	5:::::5555555555   0:::::0     0:::::00:::::0     0:::::00:::::0     0:::::0$:::::$$$$$$$$$   
+	5:::::::::::::::5  0:::::0 000 0:::::00:::::0 000 0:::::00:::::0 000 0:::::0 $$::::::::::::$$ 
+	555555555555:::::5 0:::::0 000 0:::::00:::::0 000 0:::::00:::::0 000 0:::::0   $$$$$$$$$:::::$
+	            5:::::50:::::0     0:::::00:::::0     0:::::00:::::0     0:::::0            $::::$
+	            5:::::50:::::0     0:::::00:::::0     0:::::00:::::0     0:::::0            $::::$
+	5555555     5:::::50::::::0   0::::::00::::::0   0::::::00::::::0   0::::::0$$$$$       $::::$
+	5::::::55555::::::50:::::::000:::::::00:::::::000:::::::00:::::::000:::::::0$::::$$$$$$$:::::$
+	 55:::::::::::::55  00:::::::::::::00  00:::::::::::::00  00:::::::::::::00 $::::::::::::::$$ 
+	   55:::::::::55      00:::::::::00      00:::::::::00      00:::::::::00    $$$$$$:::$$$$$   
+	     555555555          000000000          000000000          000000000           $:::$       
+	                                                                                  $$$$$       
+	'''
 
-fh = open(pathFile,'r')
-for line in fh:
-    try:
-        url = 'https://' + line.rstrip('\n')
-        print(url + '---' + str(len(url)));
-        r = requests.get(url,timeout=5)        
-        if(r.status_code==200):
-            #open chrome with location
-            numPageOpened++
-            webbrowser.get('chrome').open_new(url)
-        else:
-            print(line)
-    except requests.ConnectionError:
-        print(line + " --- failed to connect")
-fh.close()
+	print(strArt)
+	print("\n\n")
+	countLine = 0
+	fp = open(pathfile,'r')
+	numPageOpened = 0
 
-        
+	for line in fp:		
+		countLine=countLine+1
+		if (numPageOpened == numPageOpen):
+			numPageOpened = 0
+			print("\n\n\n")
+			#print("Opened: " + str(opened))
+			programPause = raw_input("Press the <ENTER> key to continue...")
+			os.system("clear")
+			print(strArt)			
+		else:			
+			try:
+				url = 'http://' + line.rstrip('\n')
+				r = requests.get(url,timeout=5)        
+				if(r.status_code==200):
+					openwebsite(line)
+					numPageOpened = numPageOpened + 1												
+				else:
+					print(line.rstrip('\n') + ' --- ' + str(countLine));			
+			except:
+				print(line.rstrip('\n') + ' --- ' + str(countLine));
+finally:
+	fp.close()
+
